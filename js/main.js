e2a38c0b9d6e8f5fd71154a04adae269a208eb99
dev <universalvn.site@gmail.com>
2024-05-13 14:01:27 +0700

    - Sửa tiêu đề trang, tạo file main js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-(function(){
-    // Back to Top - by CodyHouse.co
-	var backTop = document.getElementsByClassName('js-cd-top')[0],
-		offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
-		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		scrollDuration = 700,
-		scrolling = false;
+$('.counter').counterUp({
+    delay: 10,
+    time: 3000
+});
 
-	if( backTop ) {
-		//update back to top visibility on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
+$('.main-slider').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
 
-		//smooth scroll to top
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
-		});
-	}
-
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top--is-visible') : Util.removeClass(backTop, 'cd-top--is-visible cd-top--fade-out');
-		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top--fade-out');
-		scrolling = false;
-	}
-})();
+new WOW().init();
