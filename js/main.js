ad7397dd665fd6ad67a9ae6be16b02d263ef473d
dev <universalvn.site@gmail.com>
2024-05-18 22:37:46 +0700

    Cập nhật section Features, Number, Video, Testimonials

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
+// Number
 $('.counter').counterUp({
     delay: 10,
     time: 3000
 });
 
-$('.main-slider').owlCarousel({
+// Video
+$('.popup-video').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+
+// testimonial
+$('#testimonial-slider').owlCarousel({
     loop: true,
-    margin: 30,
-    nav: true,
+    margin: 20,
+    nav: false,
+    responsiveClass: true,
     responsive: {
         0: {
-            items: 1
+            items: 1,
+            nav: false
         },
-        600: {
-            items: 2
+        736: {
+            items: 1,
+            nav: false
         },
         1000: {
-            items: 3
+            items: 2,
+            nav: false,
+            loop: false
         }
     }
 })
